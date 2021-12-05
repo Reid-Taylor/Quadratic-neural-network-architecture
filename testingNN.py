c9bc8f19316ebd6b2ad4f7c34d121d6266e77bb4
Reid Taylor <rebridgers@gmail.com>
2021-12-05 13:23:07 -0500

    no longer necessary or relevant

--- testingNN.py
+++
@@ -1,8 +0,0 @@
-from cnnClass import convolutionalNeuralNetwork
-from qnnClass import quadraticNeuralNetwork
-import numpy as np
-
-convolute = convolutionalNeuralNetwork(dims=[8,4,8])
-data = np.random.randn(8,1) * .01
-results = np.zeros(shape=(1,8))
-convolute.train(data, results)
