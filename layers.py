229056aff24f1a51026ab4a94e6fa523f19cbe3a
Reid Taylor <rebridgers@gmail.com>
2021-12-05 13:31:26 -0500

    removing legacy class

--- layers.py
+++ layers.py
@@ -42,26 +42,6 @@ class ActivationLayer(Layer):
     def backward_propagation(self, output_error, learning_rate):
         return self.activation_prime(self.input) * output_error
 
-class QuadraticLayerMath(Layer):
-    def __init__(self, input_size, output_size):
-        self.quadWeights = np.random.rand(input_size, output_size, input_size) - 0.5
-        self.weights = np.random.rand(input_size, output_size) - 0.5
-        self.bias = np.zeros((1,output_size))
-    
-    def forward_propagation(self, input_data):
-        self.input = input_data
-        self.output = np.dot(np.tensordot(self.input, self.quadWeights, axes=1),self.input.T) + np.dot(self.input, self.weights) + self.bias
-        return self.output
-    
-    def backward_propagation(self, output_error, learning_rate):
-        input_error = np.dot(output_error, self.weights.T)
-        weights_error = np.dot(self.input.T, output_error)
-        quad_weights_error = np.dot(np.dot(self.input.T, output_error),self.input)
-        self.quadWeights -= learning_rate * quad_weights_error
-        self.weights -= learning_rate * weights_error
-        self.bias -= learning_rate * output_error
-        return input_error
-
 class QuadraticLayer(Layer):
     def __init__(self, input_size, output_size):
         self.quadWeights = np.random.rand(input_size, output_size, input_size) - 0.5
